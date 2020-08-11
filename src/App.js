bc568cbb29645e3e0265e849fc9883f451de6811
DevinKott <dak3386@rit.edu>
2020-08-11 09:08:30 -0400

    add main structure to page

--- src/App.js
+++ src/App.js
@@ -2,9 +2,20 @@ import React from 'react';
 
 function App() {
     return (
-        <main>
-            <section>
-                
+        <main
+            class='flex justify-center'
+        >
+            <section
+                class='max-w-4xl w-full p-4'
+            >
+                <h1
+                    class='text-3xl font-semibold mt-4 mb-4'
+                >
+                    Earth Meteorites
+                </h1>
+                <p>
+                    
+                </p>
             </section>
         </main>
     );
