73a98f062bf02acc685f9d555b466ae473e68899
DevinKott <dak3386@rit.edu>
2020-08-11 09:11:03 -0400

    make all text grayish, add some information

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {
     return (
         <main
-            class='flex justify-center'
+            class='flex justify-center text-gray-800'
         >
             <section
                 class='max-w-4xl w-full p-4'
@@ -14,7 +14,13 @@ function App() {
                     Earth Meteorites
                 </h1>
                 <p>
-                    
+                    NASA provides a great list of earth meteorite landings, available in JSON format.
+                    This site allows you to set your location and see the landings closest to you.
+                </p>
+                <p>
+                    You location is needed for map utilities and calculating the distance to the
+                    nearest meteorite landings. At no point is your location sent to anyone, including
+                    me. Please audit the Network Tab in your browser to verify.
                 </p>
             </section>
         </main>
