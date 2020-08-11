ba7cce55fc28334001232d6b32bae0b552700b50
DevinKott <dak3386@rit.edu>
2020-08-11 09:37:06 -0400

    change class to className

--- src/comps/RequestLocation.js
+++ src/comps/RequestLocation.js
@@ -29,30 +29,30 @@ function RequestLocation() {
 
     return (
         <section
-            class='flex flex-col'
+        className='flex flex-col'
         >
             <h3
-                class='text-xl font-semibold mt-4 mb-4'
+                className='text-xl font-semibold mt-4 mb-4'
             >
                 Step 1
             </h3>
             <span>Please click the button below to enable location tracking from your browser.</span>
             <button
-                class='shadow-md max-w-md p-4 mt-4 mb-4'
+                className='shadow-md max-w-md p-4 mt-4 mb-4'
                 onClick={buttonHandler}
             >
                 Enable Location
             </button>
             <section
-                class='flex flex-col'
+                className='flex flex-col'
             >
-                <span class='font-semibold'>Latitude: {location.latitude !== undefined ? parseFloat(location.latitude).toFixed(DECIMALS) : 'Not available.'}</span>
-                <span class='font-semibold'>Longtitude: {location.longitude !== undefined ? parseFloat(location.longitude).toFixed(DECIMALS) : 'Not available.'}</span>
+                <span className='font-semibold'>Latitude: {location.latitude !== undefined ? parseFloat(location.latitude).toFixed(DECIMALS) : 'Not available.'}</span>
+                <span className='font-semibold'>Longtitude: {location.longitude !== undefined ? parseFloat(location.longitude).toFixed(DECIMALS) : 'Not available.'}</span>
             </section>
             {
                 error &&
                 <section>
-                    <span class='text-red-700'>
+                    <span className='text-red-700'>
                         There was a problem requesting your location.
                         This application will not function if you do not allow access.
                     </span>
